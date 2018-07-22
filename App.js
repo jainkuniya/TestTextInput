3bd6d6621d20bb6f13ede84e63e6948963febc9b
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-07-22 17:50:39 +0530

    Set default text value just to test.

--- App.js
+++ App.js
@@ -12,12 +12,13 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 type Props = {};
 export default class App extends Component<Props> {
   props: Props;
+  // set default value just to test
   render() {
     return (
       <View style={styles.wrapper}>
         <View style={styles.container}>
           <Text style={styles.surroundText}>React</Text>
-          <TextInput style={styles.textInput} placeholder="Some long placeholder" />
+          <TextInput style={styles.textInput} placeholder="Some long placeholder" value="S" />
           <Text style={styles.surroundText}>Native</Text>
         </View>
       </View>
